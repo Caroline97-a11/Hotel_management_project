f451144a51afddc47d76899b25d1cd70ffe67928
Caroline Anyijukire <anyijukirecarolineck@gmail.com>
2026-04-13 21:05:18 +0300

    I have worked on form validate in forms.py and Category

--- rooms/forms.py
+++ rooms/forms.py
@@ -7,8 +7,30 @@ class RoomForm(forms.ModelForm):
         model = Room
         fields = '__all__'
 
+        error_messages ={
+            'room_number':{
+                'required':'Please enter a valid room number'}
+                }
+        error_messages ={
+            'room_category':{
+                'required':'Please enter a valid room category'}
+                }
+        
+
 
 class RoomTypeForm(forms.ModelForm):
     class Meta:
         model = RoomType
         fields = ['name', 'price']
+        error_messages ={
+            'name':{
+                'required':'Please enter a valid room number'}
+                }
+        error_messages ={
+            'price':{
+                'required':'Please enter  valid amount'}
+                }
+        
+
+
+       
